f5addcb4b886227209006707afd9387673c68b58
PhilippMatthes <github@philippmatth.es>
2018-02-11 18:25:29 +0100

    Initial Commit

---
+++ ImageRecognizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageRecognizer
+//
+//  Created by Philipp Matthes on 11.02.18.
+//  Copyright © 2018 Philipp Matthes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
